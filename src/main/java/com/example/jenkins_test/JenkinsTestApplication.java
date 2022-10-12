d8ad154fa21886023c97bfb7293985991266c4da
berin <berin.hadziabdic@genspark.net>
2022-10-12 10:39:55 -0700

    SCM polling should build this

--- src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkins_test/JenkinsTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hi from java!");
+        System.out.println("Hi from java! change here!");
         SpringApplication.run(JenkinsTestApplication.class, args);
     }
 
